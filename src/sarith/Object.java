4cc8bb6fa9a79ee67dbe961c7d232588dac726b7
Sarith <sarithkak2510@gmail.com>
2021-08-01 17:02:32 +0700

    This class.

---
+++ src/sarith/Object.java
@@ -0,0 +1,9 @@
+package sarith;
+
+public class Object {
+    public static void main(String[] args){
+        //This is show console Hello!
+        System.out.println("Hello");
+    }
+}
+
--- src/store/Main.java
+++
@@ -1,8 +0,0 @@
-package store;
-
-public class Main {
-    public static void main(String[] args){
-        //This is show console Hello world!
-        System.out.println("Hello World");
-    }
-}
