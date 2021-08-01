270e1bebebe01871a13ba8c206149a01d6f88aec
Sarith <sarithkak2510@gmail.com>
2021-08-01 16:02:25 +0700

    Create test One

---
+++ src/model/Selection.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Selection {
+    public static void main(String[] args){
+        //This is show console Hello world!
+        System.out.println("Hello World");
+    }
+}
